60663e04a2458dbb4b45cff5ef759780f05353c2
Ивайло Шопов <is41062491@edu.mon.bg>
2023-06-16 17:33:36 +0300

    Add project files.

---
+++ 2. Car Race/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Car_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int finishLine = (numbers.Count - 1) / 2;
+            float sum = 0;
+            float sum1 = 0;
+            for (int i = 0; i < finishLine; i++)
+            {
+                if (numbers[i] == 0)
+                {
+                    sum *= 0.8f;
+                }
+                else
+                {
+                    sum += numbers[i];
+                }
+            }
+            for (int i = numbers.Count-1; i > finishLine; i--)
+            {
+                if (numbers[i] == 0)
+                {
+                    sum1 *= 0.8f;
+                }
+                else
+                {
+                    sum1 += numbers[i];
+                }
+            }
+            if (sum<sum1)
+            {
+                Console.WriteLine($"The winner is left with total time: {sum}");
+            }
+            else if (sum>sum1)
+            {
+                Console.WriteLine($"The winner is right with total time: {sum1}");
+            }
+        }
+    }
+}
---
+++ RandomSentencesGenerator/RandomSentencesGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomSentencesGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Peter", "Michell", "Jane", "Steve" };
+            string[] places = { "Sofia", "Plovdiv", "Varna", "Burgas" };
+            string[] verbs = { "eats", "holds", "sees", "plays with", "brings" };
+            string[] nouns = { "stones", "cake", "apple", "laptop", "bikes" };
+            string[] adverbs = { "slowly", "diligently", "warmly", "sadly", "rapidly" };
+            string[] details = { "near the river", "at home", "in the park" };
+            Console.Write("Hello, please write how many sentences you want to generate at random: ");
+            int countOfSentences = int.Parse(Console.ReadLine());
+            int count = 1;
+            while (count<=countOfSentences)
+            {
+                string randomName = GetRandomWord(names);
+                string randomPlace = GetRandomWord(places);
+                string randomVerb = GetRandomWord(verbs);
+                string randomNoun = GetRandomWord(nouns);
+                string randomAdverb = GetRandomWord(adverbs);
+                string randomDetail = GetRandomWord(details);
+                string who = $"{randomName} from {randomPlace}";
+                string action = $"{randomAdverb} {randomVerb} {randomNoun}";
+                string sentence = $"{who} {action} {randomDetail}.";
+                Console.WriteLine(sentence);
+                count++;
+            }
+            
+
+        }
+        static string GetRandomWord(string[] words)
+        {
+            Random random = new Random();
+            int randomIndex = random.Next(words.Length);
+            string word = words[randomIndex];
+            return word;
+        }
+    }
+}
